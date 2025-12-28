cff205de2fe994135a009f21cc0f8c2867460e05
João Claro <joao9.claro99@gmail.com>
2025-12-28 19:18:19 +0000

    Set up database and entities

---
+++ MyMedia/Domain/Entities/enums/ProductStatus.cs
@@ -0,0 +1,6 @@
+﻿namespace MyMedia.Domain.Entities.enums;
+
+public enum ProductStatus
+{
+    Pending, Active
+}
--- MyMedia/Infrastructure/ApplicationDbContext.cs
+++ MyMedia/Infrastructure/ApplicationDbContext.cs
@@ -7,21 +7,20 @@ namespace MyMedia.Infrastructure;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
 {
-    public DbSet<Supplier> Companies { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<ProductCategory> ProductCategories { get; set; }
-    public DbSet<ProductSnapshot> ProductSnapshots { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<CartProduct> CartProducts { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
+        
+        /* ================ PRODUCT CATEGORY ======================== */
         modelBuilder.Entity<ProductCategory>()
             .HasKey(pc => new { pc.ProductId, pc.CategoryId });
-
+        
         modelBuilder.Entity<ProductCategory>()
             .HasOne(pc => pc.Product)
             .WithMany(p => p.ProductCategories)
@@ -31,18 +30,36 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
             .HasOne(pc => pc.Category)
             .WithMany(c => c.ProductCategories)
             .HasForeignKey(pc => pc.CategoryId);
+    
+        /* ======================== ORDER ============================= */
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.User)
+            .WithMany(u => u.Orders)
+            .HasForeignKey(o => o.UserId)
+            .IsRequired();
 
-        modelBuilder.Entity<CartProduct>()
-            .HasKey(cp => new { cp.UserId, cp.ProductId });
-
-        modelBuilder.Entity<CartProduct>()
-            .HasOne(cp => cp.User)
-            .WithMany(c => c.CartProducts)
-            .HasForeignKey(cp => cp.UserId);
-
-        modelBuilder.Entity<CartProduct>()
-            .HasOne(cp => cp.Product)
-            .WithMany(p => p.CartProducts)
-            .HasForeignKey(cp => cp.ProductId);
+        /* ==================== ORDER ITEM ============================ */
+        modelBuilder.Entity<OrderItem>()
+            .HasKey(cp => new { cp.OrderId, cp.ProductId });
+        
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(oi => oi.Order)
+            .WithMany(o=> o.OrderItems)
+            .HasForeignKey(oi => oi.OrderId)
+            .IsRequired();
+    
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(oi => oi.Product)
+            .WithMany(p => p.OrderItems)
+            .HasForeignKey(oi => oi.ProductId)
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired();
+        
+        /* ======================== SUPPLIER ============================= */
+        modelBuilder.Entity<Product>()
+            .HasOne(p => p.Supplier)
+            .WithMany(u => u.SuppliedProducts)
+            .HasForeignKey(p => p.SupplierId)
+            .IsRequired();
     }
 }
