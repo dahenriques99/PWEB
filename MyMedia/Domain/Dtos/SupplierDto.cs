9b2c99bcd55d2011f71c7f169c5247aec1060ed1
João Claro <joao9.claro99@gmail.com>
2025-12-28 15:52:54 +0000

    Add dtos

---
+++ MyMedia/Domain/Dtos/SupplierDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMedia.Domain.Dtos;
+
+public class SupplierDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string NIF { get; set; } = string.Empty;
+    public byte[] LogoData { get; set; } = [];
+}
---
+++ MyMedia/Domain/Dtos/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MyMedia.Domain.Dtos;
+
+public class UserDto
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+}
