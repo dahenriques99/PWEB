a49205472bc881c83a94180673a6efe16deb4a7a
João Claro <joao9.claro99@gmail.com>
2025-12-30 17:20:59 +0000

    Add basic user and product pages

---
+++ RCL/Services/ApiService.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.Http.Json;
+using RCL.Dtos;
+
+namespace RCL.Services;
+
+public class ApiService(HttpClient httpClient)
+{
+    public async Task<IEnumerable<ProductCategoryDto>?> GetAllProductsWithCategories()
+    {
+        try
+        {
+            var response = await httpClient.GetFromJsonAsync<IEnumerable<ProductCategoryDto>>("api/ProductCategory/products");
+            return response;
+        }
+        catch (Exception ex)
+        {
+            await Console.Error.WriteLineAsync($"Error fetching products with categories: {ex.Message}");
+            return Enumerable.Empty<ProductCategoryDto>();
+        }
+    }
+
+    public async Task<IEnumerable<CategoryDto>?> GetAllCategories()
+    {
+        try
+        {
+            var response = await httpClient.GetFromJsonAsync<IEnumerable<CategoryDto>>("api/Category");
+            return response;
+        }
+        catch (Exception ex)
+        {
+            await Console.Error.WriteLineAsync($"Error fetching categories: {ex.Message}");
+            return Enumerable.Empty<CategoryDto>();
+        }
+    }
+
+    public async Task<IEnumerable<SupplierDto>?> GetAllCompanies()
+    {
+        try
+        {
+            var response = await httpClient.GetFromJsonAsync<IEnumerable<SupplierDto>>("api/Company");
+            return response;
+        }
+        catch (Exception ex)
+        {
+            await Console.Error.WriteLineAsync($"Error fetching companies: {ex.Message}");
+            return Enumerable.Empty<SupplierDto>();
+        }
+    }
+
+    public async Task AddToCartAsync(CartDto dto, string token)
+    {
+        try
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            var response = await httpClient.PostAsJsonAsync("api/Order/cart", dto);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception ex)
+        {
+            await Console.Error.WriteLineAsync($"Error adding product to cart: {ex.Message}");
+            throw;
+        }
+    }
+
+    public async Task<UserDto?> GetUserInfo(string email, string token)
+    {
+        try
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            var response = await httpClient.GetFromJsonAsync<UserDto>($"api/User/email/{email}");
+            return response;
+        }
+        catch (Exception ex)
+        {
+            await Console.Error.WriteLineAsync($"Error fetching client info: {ex.Message}");
+            return null;
+        }
+    }
+}
