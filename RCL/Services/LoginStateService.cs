42049b4412c601992b752ec4c13316e105453f61
João Claro <joao9.claro99@gmail.com>
2025-12-30 17:20:59 +0000

    Add basic user and product pages

---
+++ RCL/Services/LoginStateService.cs
@@ -0,0 +1,27 @@
+﻿namespace RCL.Services;
+
+public abstract class LoginStateService(CartService cartService)
+{
+    public static bool IsLoggedIn { get; private set; }
+
+    // Event to notify the components when the login state changes
+    public static event Action? OnLoginStateChanged;
+
+    // Method to log in
+    public async Task LogIn()
+    {
+        IsLoggedIn = true;
+        await cartService.SyncCartAfterLogin(); // Sync cart after login
+        NotifyStateChanged();
+    }
+
+    // Method to log out
+    public static void LogOut()
+    {
+        IsLoggedIn = false;
+        NotifyStateChanged();
+    }
+
+    // Notify any listeners (e.g., Navbar component) that the state has changed
+    private static void NotifyStateChanged() => OnLoginStateChanged?.Invoke();
+}
