f02c359453371188a4c290cdab74e29bc5e0c504
João Claro <joao9.claro99@gmail.com>
2026-01-04 01:45:12 +0000

    Make pages more beautiful

--- RCL/Dtos/Request/CheckoutRequestDto.cs
+++ RCL/Dtos/Request/CheckoutRequestDto.cs
@@ -3,6 +3,7 @@
 public sealed class CheckoutRequestDto
 {
     public List<CheckoutItemDto> Items { get; set; } = new();
+    public string? DeliveryMode { get; set; }
 }
 
 public sealed class CheckoutItemDto
